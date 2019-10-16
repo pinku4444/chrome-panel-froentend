44dcd9968000145b104fae851249c7925afa09a0
pinku <Pinku.Kumar@dentsuaegis.com>
2019-10-16 19:25:45 +0530

    added validation

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,11 @@ import axios from 'axios';
 import config from '../config/config';
 
 const API = axios.create({
-    baseURL : config.uri
+    baseURL : config.uri,
+    headers : {
+        'Content-Type': 'application/json',
+        'authorization': localStorage.getItem('authToken')
+    }
 });
 
 
